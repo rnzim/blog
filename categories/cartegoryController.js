4027ace6de5eb3ba7054a69a8c85637f8123ccb2
rnzim <rnxpphp@gamil.com>
2023-10-02 12:26:00 -0300

    categories added

--- categories/cartegoryController.js
+++ categories/cartegoryController.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('./Category')
+const slugify = require('slugify')
 
-router.get('/category',(req,res)=>{
-    res.send('rota de cartegoria')
+router.get('/admin/categories/new',(req,res)=>{
+    res.render('admin/categories/new.ejs')
 })
 
-router.get('/newCategory',(req,res)=>{
-    res.send('rota para nova cartegoria')
+router.post('/cartegories/save',(req,res) =>{
+     var title = req.body.title
+     if(title != undefined){
+        Category.create({
+            title:title,
+            slug:slugify(title)
+        }).then(()=>{
+            res.redirect('/')
+        })
+     }else{
+        res.redirect('/admin/categories/new')
+     }
 })
-
+router.get('/admin/categories/',(req,res)=>{
+    Category.findAll({
+       raw:true, order:[['id','desc']]
+    }).then((cartegories)=>{
+        cartegories.forEach(cartegories => {
+            console.log(cartegories.title)
+        })
+    })
+    res.render('admin/categories')
+})
 module.exports = router
