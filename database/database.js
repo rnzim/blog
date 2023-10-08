b707dd6d6eddda7d730137f6bbb718d9f61f838c
Raimundo Nonato <144294493+rnzim@users.noreply.github.com>
2023-10-07 21:14:49 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize('blogxp','root','',{
-    host:'localhost',
+const connection = new Sequelize('blogxp','root','Iu9byg8sxRMnPVmfVeO2',{
+    host:'containers-us-west-100.railway.app',
     dialect:'mysql',
-    timezone: '-03:00'
+    timezone: '-03:00',
+    port: 7262
 })
 
 module.exports = connection
